732c429c2972e586f951047f18917f0eea6e6a2b
Aditya Banka <adityabanka1947@gmail.com>
2025-04-17 03:37:05 +0530

    minor ui improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,15 +42,15 @@ function App() {
 
       {/* Body */}
       <div>
-        <div className="w-fit pt-40 mx-[10%] md:mx-[20%] flex flex-col items-center justify-center">
+        <div className="w-fit pt-10 md:pt-40 mx-[10%] md:mx-[20%] flex flex-col items-center justify-center">
 
           <div className="flex flex-col items-start px-[5%]">
             <p className="text-4xl font-zilla-slab-bold my-5">Let's <span className="text-rose-400 text-5xl">Heal</span> our minds together</p>
             <p className="font-zilla-slab-regular">We build the world's first certified AI mental healthcare platform. Our model is trained on data from board certified healthcare professionals.</p>
           </div>
 
-          <div className="w-full flex items-center justify-center m-5 p-5 bg-white/50 rounded-full shadow-sm border border-rose-400 space-x-5">
-            <input type="text" placeholder="Ask our experts anything" className="border border-rose-400 w-full p-5 rounded-full"></input>
+          <div className="w-full flex items-center justify-center m-5 p-2 md:p-5 bg-white/50 rounded-full shadow-sm border border-rose-400 space-x-1 md:space-x-5">
+            <input type="text" placeholder="Ask our expert model anything.." className="border border-rose-400 w-full p-5 rounded-full"></input>
             <button className="p-4 rounded-full hover:bg-rose-400 hover:text-white font-zilla-slab-italic text-lg border border-rose-400 transition-all duration-200">Send</button>
           </div>
         </div>
