1d4726faaffa714bf48ee541f8aa4bad2d8de7f6
Aditya Banka <adityabanka1947@gmail.com>
2025-04-19 05:52:16 +0530

    text fix

--- frontend/src/components/TextBox.tsx
+++ frontend/src/components/TextBox.tsx
@@ -6,20 +6,20 @@ import { TextEffect } from './motion-primitives/text-effect';
 type TextBoxProps = {
     user: boolean;
     children: React.ReactNode;
-  };
+};
 const TextBox: React.FC<TextBoxProps> = ({ user, children }) => {
-  return (
-    <div className={`flex space-x-5 space-y-5 ${user ? "flex-row-reverse space-x-reverse" : ""} `}>
-      <Avatar>
-        {!user ? (
-          <AvatarImage src={thumbnail} alt="User Avatar" />
-        ) : null}
-        <AvatarFallback className="bg-rose-200 text-white">{user ? "U" : "Ai"}</AvatarFallback>
-      </Avatar>
-      <TextEffect per="word" preset="scale" speedReveal={1} speedSegment={2} className="text-xl font-zilla-slab-regular text-justify">{children}</TextEffect>
-      <div className="px-5"></div>
-    </div>
-  );
+    return (
+        <div className={`flex space-x-5 space-y-5 ${user ? "flex-row-reverse space-x-reverse" : ""} `}>
+            <Avatar>
+                {!user ? (
+                    <AvatarImage src={thumbnail} alt="User Avatar" />
+                ) : null}
+                <AvatarFallback className="bg-rose-200 text-white">{user ? "U" : "Ai"}</AvatarFallback>
+            </Avatar>
+            <TextEffect per="word" preset="scale" speedReveal={1} speedSegment={2} className="text-xl font-zilla-slab-regular text-justify">{String(children)}</TextEffect>
+            <div className="px-5"></div>
+        </div>
+    );
 };
 
 export default TextBox;
